a7f5986b946e516d197545cf342917441dcf8cb6
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-09-04 16:35:47 +0100

    Change degrees to a variable

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -5,20 +5,21 @@ const allHands = document.querySelectorAll('.hand');
 
 function setDate() {
   const now = new Date();
+  const startPos = 90;
 
   const sec = now.getSeconds();
-  const secDegrees = ((sec / 60) * 360) + 90;
+  const secDegrees = ((sec / 60) * 360) + startPos;
   secHand.style.transform = `rotate(${secDegrees}deg)`;
 
   const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((sec / 60) * 6) + 90;
+  const minsDegrees = ((mins / 60) * 360) + ((sec / 60) * 6) + startPos;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+  const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + startPos;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
-  if(secDegrees === 90) {
+  if(secDegrees === startPos) {
     allHands.forEach(hand => hand.style.transition = 'none')
   } else {
     allHands.forEach(hand => hand.style.transition = '')
